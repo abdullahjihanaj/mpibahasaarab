1efeb82c6bc2c86f311c33794036c90b46bd6840
abdullahjihanaj <abdullahjihanaj@gmail.com>
2025-05-06 21:38:47 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,194 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5nNKyaY7Rmp":
+        Script1();
+        break;
+      case "6KADzK28WvO":
+        Script2();
+        break;
+      case "5VldXai2H45":
+        Script3();
+        break;
+      case "6X82khG6hZw":
+        Script4();
+        break;
+      case "6kMMjxwNuXT":
+        Script5();
+        break;
+      case "651zXMeZgqw":
+        Script6();
+        break;
+      case "5njb7xRAl8p":
+        Script7();
+        break;
+      case "6Le3IPV2KNe":
+        Script8();
+        break;
+      case "645Ptv9BydW":
+        Script9();
+        break;
+      case "6efTYVsCnTu":
+        Script10();
+        break;
+      case "6b5KRSC5mME":
+        Script11();
+        break;
+      case "6CH5FQgDRza":
+        Script12();
+        break;
+      case "5iCyKYj8aTv":
+        Script13();
+        break;
+      case "5YdKxoWRGd3":
+        Script14();
+        break;
+      case "5dMMeUjCu9n":
+        Script15();
+        break;
+      case "6jRGhM7b6d6":
+        Script16();
+        break;
+      case "5WyrTcON02r":
+        Script17();
+        break;
+      case "6igSvMm7Y6c":
+        Script18();
+        break;
+      case "6jrv4v6VTFH":
+        Script19();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="satu.mp3";
+audio.load();
+audio.play();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="satu.mp3";
+audio.load();
+audio.pause();
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="satu.mp3";
+audio.load();
+audio.play();
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.0;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.1;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.2;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.3;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.4;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.6;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.7;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.8;
+}
+
+function Script13()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.9;
+}
+
+function Script14()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 1.0;
+}
+
+function Script15()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="satu.mp3";
+audio.load();
+audio.play();
+}
+
+function Script16()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="dua.mp3";
+audio.load();
+audio.play();
+}
+
+function Script17()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="tiga.mp3";
+audio.load();
+audio.play();
+}
+
+function Script18()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="empat.mp3";
+audio.load();
+audio.play();
+}
+
+function Script19()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="terakhir.mp3";
+audio.load();
+audio.play();
+}
+
